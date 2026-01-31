1930fa674702d0c84737d183b897758d96c636c6
Nick Nedjat <nick.nedjat@icloud.com>
2026-01-31 17:18:56 -0500

    Initial Commit

---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by Nick Nedjat on 31.01.26.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
